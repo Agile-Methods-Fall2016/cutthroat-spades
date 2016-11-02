9ea62db6b873af27f81db8c14343e36b4e98854e
Kirska <kpatterson67@gmail.com>
2016-11-02 15:58:22 -0500

    Connecting to server

--- Menu.py
+++ Menu.py
@@ -1,5 +1,7 @@
 from CutthroatGame import main_game
 from Tkinter import Tk, Label, Button, Entry, Message
+import socket
+import json
 
 
 class SpadesMenu:
@@ -13,7 +15,7 @@ class SpadesMenu:
         self.labelTitle.grid(row=current_row, columnspan=column_span)
         current_row += 1
 
-        self.labelServerIP = Label(master, text="IP of Server:")
+        self.labelServerIP = Label(master, text="Server host:")
         self.labelServerIP.grid(row=current_row, column=0)
 
         self.entryServerIP = Entry(master)
@@ -33,11 +35,34 @@ class SpadesMenu:
         self.close_button.grid(row=current_row, columnspan=column_span)
 
         self.server_ip = ""
+        self.connected = False
+
+        # create a socket object
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 
     def connect_to_server(self):
         self.server_ip = self.entryServerIP.get()
         self.connection_message_text += "Connecting to server at " + self.server_ip + "\n"
+        self.connection_message.configure(text=self.connection_message_text)
+
+        port = 9999
+
+        # connection to hostname on the port.
+        self.s.connect((self.server_ip, port))
+        self.s.settimeout(3)
+
+        # Receive no more than 1024 bytes
+        response = self.s.recv(1024)
+
+        # check connection response
+        response_json = json.loads(response)
+        if response_json['connected'] == 'true':
+            self.connection_message_text += "Connected.\n"
+            self.connected = True
+        else:
+            self.connection_message_text += "Connection failed.\n"
+
         self.connection_message.configure(text=self.connection_message_text)
 
 
